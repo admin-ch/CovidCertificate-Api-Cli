fd62f54300d5589d31c5a74740f74484c8ba9efb
Felix Deierlein <felix.deierlein@bit.admin.ch>
2021-05-19 15:32:59 +0200

    Fix canonicalization

--- src/signing/signer.ts
+++ src/signing/signer.ts
@@ -5,9 +5,7 @@ export class CanonicalSha256WithRsaSigner {
   constructor(private privateKeyObject: crypto.KeyObject) {}
 
   sign(message: string): string {
-    // the canonicalization regex is defined by the API
-    const regex = /[\n\t ]/
-    const canonicalMessage =  message.replace(regex, '')
+    const canonicalMessage = CanonicalSha256WithRsaSigner.canonicalize(message)
 
     const bytes = Buffer.from(canonicalMessage, 'utf8')
     const sign = crypto.createSign('RSA-SHA256')
@@ -17,8 +15,19 @@ export class CanonicalSha256WithRsaSigner {
     return base64encodedSignature
   }
 
+  static canonicalize(message: string): string {
+    // the canonicalization regex is defined by the API
+    const regex = /[\n\t]/gm
+    const canonicalMessage = message.replace(regex, '')
+    return canonicalMessage
+  }
+
   static fromKeyFile(keyFile: string): CanonicalSha256WithRsaSigner {
     const pemEncodedKey = fs.readFileSync(keyFile)
+    return this.fromPemEncodedKey(pemEncodedKey)
+  }
+
+  static fromPemEncodedKey(pemEncodedKey: Buffer) {
     const privateKeyObject = crypto.createPrivateKey(pemEncodedKey)
     return new CanonicalSha256WithRsaSigner(privateKeyObject)
   }
--- test/signing/signer.test.ts
+++ test/signing/signer.test.ts
@@ -1,5 +1,6 @@
 import {resourceFilePath} from '../utils'
 import {CanonicalSha256WithRsaSigner} from '../../src/signing/signer'
+import {expect} from '@oclif/test'
 
 describe('CanonicalSha256WithRsaSigner', () => {
   context('a valid keyFile', () => {
@@ -7,6 +8,21 @@ describe('CanonicalSha256WithRsaSigner', () => {
       const keyFilePath = resourceFilePath('dev.local.key')
       const signer = CanonicalSha256WithRsaSigner.fromKeyFile(keyFilePath)
       signer.sign('abc')
+    })
+  })
+  describe('normalization', () => {
+    it('works like the backend', () => {
+      const message = '{\n' +
+        '\t"name":{\n' +
+        '\t\t"familyName":"Muster",\n' +
+        '\t\t"givenName":"Hans"\n' +
+        '\t}\n' +
+        '}'
+      const expected = '{"name":{"familyName":"Muster","givenName":"Hans"}}'
+
+      const actual = CanonicalSha256WithRsaSigner.canonicalize(message)
+
+      expect(actual).to.equal(expected)
     })
   })
 })
