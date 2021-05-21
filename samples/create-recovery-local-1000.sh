160884290f165f05aba3ef4bb9f64ce2bd954654
Felix Deierlein <felix.deierlein@bit.admin.ch>
2021-05-21 22:34:30 +0200

    Update sample to use environment variables.

--- samples/create-recovery-local-1000.sh
+++ samples/create-recovery-local-1000.sh
@@ -1,17 +1,21 @@
 #!/usr/bin/env sh
 
+# get via the WebUI
+export CC_CLI_OTP=yJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJqdGkiOiJiNzNmZjVjNy03MWI0LTRjYmEtYmQ1MC1hZTdhNzlhODQ0NDEiLCJpc3MiOiJodHRwczovL2NvdmlkY2VydGlmaWNhdGUtbWFuYWdlbWVudC1kLmJhZy5hZG1pbi5jaCIsImlhdCI6MTYyMTQ5NTQ5OSwibmJmIjoxNjIxNDk1NDk5LCJzY29wZSI6ImNvdmlkY2VydGNyZWF0aW9uIiwidXNlcmV4dGlkIjoiMTIzNDU2IiwiaWRwc291cmNlIjoiRS1JRCBDSC1MT0dJTiIsImV4cCI6MTYyMTUzODY5OX0.RjC7eP3Sd41LnhoMU3sJhx5v6Ab8-tKyAxQutPbmbdQN8_qC4Ok85wo5Yjvq66N2YzxdfWS_hH8lrl6FAzL1JhYV92VvOTGCXnHqRAjzu7J_vSqkfNmqPQS8AZLXuFDL4Mfg6syyQlEJqNvDtT_RKaTJ9PhHneFxxqiOAC3Pzrw7Sr_UvFrIBF7lRx8YWYZYEEGegpuomz_BHc4yFitXY0y_d4gWahPYaTZiRqga1D2A1_HNuXOuHBHnFuaNNnwjKDG8HG_HFTsv_e-Ubo7AIWnFTUWYQsFluEZPbmhH72AeLiDJoQPRbX3HIDrpEazRBeInJv49N8z26uys_mzLUQ
+
+export CC_CLI_BASE_URL="http://localhost:8121/"
+export CC_CLI_CERTIFICATE_FILE="ZH-spital-A-t.bit.admin.ch.cer"
+export CC_CLI_KEY_FILE="ZH-spital-A-t.bit.admin.ch.key"
+export CC_CLI_OUT_DIR="out"
+# to mock WSG certificate headers
+export CC_CLI_LOCAL=true
+
 cc-cli create-recovery \
-    --baseUrl="http://localhost:8121" \
-    --certificateFile="ZH-spital-A-t.bit.admin.ch.cer" \
-    --keyFile="ZH-spital-A-t.bit.admin.ch.key" \
-    --outDir="out" \
-    --otp="eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJqdGkiOiJiNzNmZjVjNy03MWI0LTRjYmEtYmQ1MC1hZTdhNzlhODQ0NDEiLCJpc3MiOiJodHRwczovL2NvdmlkY2VydGlmaWNhdGUtbWFuYWdlbWVudC1kLmJhZy5hZG1pbi5jaCIsImlhdCI6MTYyMTQ5NTQ5OSwibmJmIjoxNjIxNDk1NDk5LCJzY29wZSI6ImNvdmlkY2VydGNyZWF0aW9uIiwidXNlcmV4dGlkIjoiMTIzNDU2IiwiaWRwc291cmNlIjoiRS1JRCBDSC1MT0dJTiIsImV4cCI6MTYyMTUzODY5OX0.RjC7eP3Sd41LnhoMU3sJhx5v6Ab8-tKyAxQutPbmbdQN8_qC4Ok85wo5Yjvq66N2YzxdfWS_hH8lrl6FAzL1JhYV92VvOTGCXnHqRAjzu7J_vSqkfNmqPQS8AZLXuFDL4Mfg6syyQlEJqNvDtT_RKaTJ9PhHneFxxqiOAC3Pzrw7Sr_UvFrIBF7lRx8YWYZYEEGegpuomz_BHc4yFitXY0y_d4gWahPYaTZiRqga1D2A1_HNuXOuHBHnFuaNNnwjKDG8HG_HFTsv_e-Ubo7AIWnFTUWYQsFluEZPbmhH72AeLiDJoQPRbX3HIDrpEazRBeInJv49N8z26uys_mzLUQ" \
     --language="de" \
     --familyName="Federer" \
     --givenName="Roger" \
     --dateOfBirth="1981-08-08" \
     --dateOfFirstPositiveTestResult="2020-01-01" \
     --countryOfTest="CH" \
-    --local \
     --testInstances=100
 
--- samples/create-recovery.sh
+++ samples/create-recovery.sh
@@ -1,15 +1,20 @@
 #!/usr/bin/env sh
 
+# get via the WebUI
+export CC_CLI_OTP=eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJqdGkiOiI1MDNkNWY4OS03NGI0LTQxZGUtYTQyNC03MTY3ZWE0NTU4ZTkiLCJpc3MiOiJodHRwczovL2NvdmlkY2VydGlmaWNhdGUtbWFuYWdlbWVudC1kLmJhZy5hZG1pbi5jaCIsImlhdCI6MTYyMTYxOTM5MSwibmJmIjoxNjIxNjE5MzkxLCJzY29wZSI6ImNvdmlkY2VydGNyZWF0aW9uIiwidXNlckV4dElkIjoiMTIzNDU2IiwiaWRwc291cmNlIjoiRS1JRCBDSC1MT0dJTiIsImV4cCI6MTYyMTY2MjU5MX0.DlgPntBVYzwTF3IpUuWLQR9oaggKBYW2Eu-3piHA3fEs9MQBAFOyheKn7LGSqMSM8Zz2lez3OY8MFwzqyDliDh4R3P8Au3vSh3AHAusfE1Vm50l9-5247ty55E_5yMcsmp6fw5aymQBpEWwBcHjKBjD4LJ8A-yCXNLJACuONyFfQoDf-2KQM1XmBHKKWVB3huqiBRlb7p6Q7bKOoYorDPEW4P7O_Ciz0WfC6O26SudDRf5tydgflNV7XXJBIVCzR1Jr6NK9MH4MneG8_D48_4Rowzemih6lJCX5pAZXDj6xaOk0RAYXr51HijG8tCNhb6H8uxYTP2PN4NzvEk7VDnw
+
+export CC_CLI_BASE_URL="https://ws.covidcertificate-r.bag.admin.ch/"
+export CC_CLI_CERTIFICATE_FILE="ZH-spital-A-t.bit.admin.ch.cer"
+export CC_CLI_KEY_FILE="ZH-spital-A-t.bit.admin.ch.key"
+export CC_CLI_OUT_DIR="out"
+
+# to mock WSG certificate headers
+#export CC_CLI_LOCAL=true
+
 cc-cli create-recovery \
-    --baseUrl="https://ws.covidcertificate-r.bag.admin.ch/" \
-    --certificateFile="ZH-spital-A-t.bit.admin.ch.cer" \
-    --keyFile="ZH-spital-A-t.bit.admin.ch.key" \
-    --outDir="out" \
-    --otp="eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJqdGkiOiJiNzNmZjVjNy03MWI0LTRjYmEtYmQ1MC1hZTdhNzlhODQ0NDEiLCJpc3MiOiJodHRwczovL2NvdmlkY2VydGlmaWNhdGUtbWFuYWdlbWVudC1kLmJhZy5hZG1pbi5jaCIsImlhdCI6MTYyMTQ5NTQ5OSwibmJmIjoxNjIxNDk1NDk5LCJzY29wZSI6ImNvdmlkY2VydGNyZWF0aW9uIiwidXNlcmV4dGlkIjoiMTIzNDU2IiwiaWRwc291cmNlIjoiRS1JRCBDSC1MT0dJTiIsImV4cCI6MTYyMTUzODY5OX0.RjC7eP3Sd41LnhoMU3sJhx5v6Ab8-tKyAxQutPbmbdQN8_qC4Ok85wo5Yjvq66N2YzxdfWS_hH8lrl6FAzL1JhYV92VvOTGCXnHqRAjzu7J_vSqkfNmqPQS8AZLXuFDL4Mfg6syyQlEJqNvDtT_RKaTJ9PhHneFxxqiOAC3Pzrw7Sr_UvFrIBF7lRx8YWYZYEEGegpuomz_BHc4yFitXY0y_d4gWahPYaTZiRqga1D2A1_HNuXOuHBHnFuaNNnwjKDG8HG_HFTsv_e-Ubo7AIWnFTUWYQsFluEZPbmhH72AeLiDJoQPRbX3HIDrpEazRBeInJv49N8z26uys_mzLUQ" \
     --language="de" \
     --familyName="Federer" \
     --givenName="Roger" \
     --dateOfBirth="1981-08-08" \
     --dateOfFirstPositiveTestResult="2020-01-01" \
-    --countryOfTest="CH" \
-
+    --countryOfTest="CH"
